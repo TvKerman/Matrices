e6fa6973db2561d08e38f86ebdcc8e28afffeb8b
Kerman <grigorij.perfilin@mail.ru>
2022-02-11 18:03:00 +0300

    added functions memory usage of matrix

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -5,4 +5,23 @@
 #ifndef MATRICES_MATRIX_H
 #define MATRICES_MATRIX_H
 
+typedef struct matrix {
+    int **values;       // Элементы матрицы
+    int nRows;          // Количество рядов
+    int nCols;          // Количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+matrix getMemMatrix(int nRows, int nCols);
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+void freeMemMatrix(matrix m);
+
+void freeMemMatrices(matrix *ms, int nMatrices);
+
 #endif //MATRICES_MATRIX_H
